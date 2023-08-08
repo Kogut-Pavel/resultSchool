b1eea192414188449cfe721e7bb2740f401d4eb7
Pavel Kogut <kogut.kogut1@mail.ru>
2023-08-08 13:11:39 +0500

    01-base-start completed

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -1,4 +1,3 @@
-
 // Task #1
 
 var a = 5;
@@ -14,3 +13,69 @@ console.log(a, b, c);
 
 // Task #2
 
+// const a = 100;
+// a = 20;
+// console.log(a);
+
+// const a = 100;
+// a = 20;
+// console.log(a);
+
+// const a = 100;
+// a = 20;
+// console.log(a);
+
+// Task #3
+
+const productName = "Кроссовки";
+const productPrice = 5000;
+const productQuantity = 3;
+const resultCost = productPrice * productQuantity;
+const resultMessage = `Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`;
+
+console.log(resultMessage);
+
+// Task #4
+
+console.log(
+    typeof "Hello",
+    typeof 10,
+    typeof {},
+    typeof null,
+    typeof undefined,
+    typeof true,
+    typeof [],
+    typeof (() => {})
+);
+
+// Task #5
+
+console.log(
+    10 == 10,
+    10 != 10,
+    12 == "Привет",
+    10 < 11,
+    12 > 20,
+    true && true,
+    true && false,
+    false && true,
+    false || true,
+    true || false,
+    true || true,
+    10 <= 10,
+    18 <= 20,
+    50 >= 50,
+    51 >= 50,
+    {} === {},
+    {} == {}
+);
+
+const a = {};
+const b = a;
+console.log(a === b);
+
+// Task #6;
+
+const a = 5;
+const b = 10;
+console.log(a > b);
